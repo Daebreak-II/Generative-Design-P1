f6f727b3c4c31bc65701dd0de724edeaebe49e28
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2022-06-23 22:41:52 -0700

    new story

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ const fillers = {
     loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
     baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
     message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
+
+    describer: ["chipper"],
+    name: ["dale"],
+    place: ["rocky mountain"],
+    weather: ["bright", "clear"],
+    thing1: ["pocket knife"],
+    thing2: ["survival book"],
+    creature: ["rabbit"],
+    surrounding: ["rock"],
+    product: ["spear"],
+
     
   };
   
@@ -18,6 +29,18 @@ const fillers = {
   
   It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
   `;
+
+  const template2 = `One day, my $describer friend $name was going out for a hike at $place. It was a $weather and $weather day, until he realized that he was completely and utterly lost!
+
+  Panicking a little, he managed to calm himself down by breathing camly, and taking stock of his surroundings. 
+  
+  In his pockets, he had a $thing1, a $thing2, and a $product. Around him, there was a $creature and a $surrounding. 
+
+  Using only $thing1 and $thing2, he managed to make a $thing1 - $product, and used it to defend himself from $creature.
+
+  Shortly Afterwards, he was rescued, 
+  
+  `
   
   
   // STUDENTS: You don't need to edit code below this line.
